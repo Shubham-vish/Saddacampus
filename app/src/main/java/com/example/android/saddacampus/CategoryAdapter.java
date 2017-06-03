a58b5b7c2b0e7c867fe2cf9cd94383ccf0de8721
Shubham Vishwakarma <shubhamvishwakarma2001@gmail.com>
2017-06-03 20:22:57 +0530

    Added listener for Category List

--- app/src/main/java/com/example/android/saddacampus/CategoryAdapter.java
+++ app/src/main/java/com/example/android/saddacampus/CategoryAdapter.java
@@ -23,6 +23,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -31,24 +32,47 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Number
 
     private static final String TAG = CategoryAdapter.class.getSimpleName();
 
-  private ArrayList<Category> mCategoryList;
+    private ArrayList<Category> mCategoryList;
+    private ArrayList<Word> wordlist;
+    int activity=0;
+    private int mColorResourceId;
 
 
     public CategoryAdapter(ArrayList<Category> c) {
         mCategoryList=c;
     }
 
+    public CategoryAdapter(ArrayList<Word> w, int m)
+    {
+        wordlist=w;
+        activity=m;
+    }
+
     @Override
     public NumberViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         Context context = viewGroup.getContext();
-        int layoutIdForListItem = R.layout.s;
-        LayoutInflater inflater = LayoutInflater.from(context);
-        boolean shouldAttachToParentImmediately = false;
+        if(activity==1)
+        {
+            int layoutIdForListItem = R.layout.list_item;
+            LayoutInflater inflater = LayoutInflater.from(context);
+            boolean shouldAttachToParentImmediately = false;
 
-        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
-        NumberViewHolder viewHolder = new NumberViewHolder(view);
+            View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+            NumberViewHolder viewHolder = new NumberViewHolder(view);
 
-        return viewHolder;
+            return viewHolder;
+        }
+        else {
+
+            int layoutIdForListItem = R.layout.s;
+            LayoutInflater inflater = LayoutInflater.from(context);
+            boolean shouldAttachToParentImmediately = false;
+
+            View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+            NumberViewHolder viewHolder = new NumberViewHolder(view);
+
+            return viewHolder;
+        }
     }
 
 
@@ -61,25 +85,46 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Number
 
     @Override
     public int getItemCount() {
+        if(activity==1){
+            return wordlist.size();
+        }
         return mCategoryList.size();
     }
 
 
-    class NumberViewHolder extends RecyclerView.ViewHolder {
+    class NumberViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         // Will display the position in the list, ie 0 through getItemCount() - 1
         TextView listItemNumberView;
         ImageView image;
-
+        TextView too;
 
         public NumberViewHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
+            if(activity==1){
+                image = (ImageView) itemView.findViewById(R.id.i) ;
+                listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+                too = (TextView) itemView.findViewById(R.id.default_text_view);
+            }
+            else
+            {
             image = (ImageView) itemView.findViewById(R.id.i) ;
             listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+            too = (TextView) itemView.findViewById(R.id.default_text_view);
+         }
         }
 
-
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(view.getContext(), "position = " + getPosition(), Toast.LENGTH_SHORT).show();
+        }
         void bind(int listIndex) {
+            if(activity==1){
+                image.setImageResource(wordlist.get(listIndex).getImageResourceId());
+                listItemNumberView.setText(wordlist.get(listIndex).getDefaultTranslation());
+                too.setText(wordlist.get(listIndex).getMiwokTranslation());
+            }
             image.setImageResource(mCategoryList.get(listIndex).getImageId());
             listItemNumberView.setText(mCategoryList.get(listIndex).getCategoryName());
         }
